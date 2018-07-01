49e0a020ff389b9a7dbbf1cfcd7fd57084dc9fd4
Martin <martin-man2010@hotmail.co.uk>
2018-07-02 00:02:04 +0100

    Added Wrapper For Maze

--- js/index.js
+++ js/index.js
@@ -8,35 +8,39 @@ var difficulty;
 // sprite.src = 'media/sprite.png';
 
 window.onload = function () {
-    if (window.innerHeight < window.innerWidth) {
-        ctx.canvas.width = window.innerHeight - (75 + (window.innerHeight / 100));
-        ctx.canvas.height = window.innerHeight - (75 + (window.innerHeight / 100));
+    let viewWidth = $("#view").width();
+    let viewHeight = $("#view").height();
+    if (viewHeight < viewWidth) {
+        ctx.canvas.width = viewHeight - (viewHeight / 100);
+        ctx.canvas.height = viewHeight - (viewHeight / 100);
     } else {
-        ctx.canvas.width = window.innerWidth - (75 + (window.innerWidth / 100));
-        ctx.canvas.height = window.innerWidth - (75 + (window.innerWidth / 100));
+        ctx.canvas.width = viewWidth - (viewWidth / 100);
+        ctx.canvas.height = viewWidth - (viewWidth / 100);
     }
 
     //Load and edit sprites
     sprite = new Image();
-    sprite.src = "/media/sprite.png"
+    sprite.src = "/media/sprite.png";
     sprite.onload = function () {
-        sprite = changeBrightness(1.20, sprite)
-    }
+        sprite = changeBrightness(1.20, sprite);
+    };
     finishSprite = new Image();
-    finishSprite.src = "/media/finishSprite.png"
+    finishSprite.src = "/media/finishSprite.png";
     finishSprite.onload = function () {
-        finishSprite = changeBrightness(1.10, finishSprite)
-    }
+        finishSprite = changeBrightness(1.10, finishSprite);
+    };
 
 };
 
 window.onresize = function () {
-    if (window.innerHeight < window.innerWidth) {
-        ctx.canvas.width = window.innerHeight - (75 + (window.innerHeight / 100));
-        ctx.canvas.height = window.innerHeight - (75 + (window.innerHeight / 100));
+    let viewWidth = $("#view").width();
+    let viewHeight = $("#view").height();
+    if (viewHeight < viewWidth) {
+        ctx.canvas.width = viewHeight - (viewHeight / 100);
+        ctx.canvas.height = viewHeight - (viewHeight / 100);
     } else {
-        ctx.canvas.width = window.innerWidth - (75 + (window.innerWidth / 100));
-        ctx.canvas.height = window.innerWidth - (75 + (window.innerWidth / 100));
+        ctx.canvas.width = viewWidth - (viewWidth / 100);
+        ctx.canvas.height = viewWidth - (viewWidth / 100);
     }
     cellSize = mazeCanvas.width / difficulty;
     if (player != null) {
